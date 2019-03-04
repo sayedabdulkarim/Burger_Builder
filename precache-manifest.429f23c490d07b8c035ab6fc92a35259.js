19075ec279e25a6316ec1e38ea0052546266fbd9
Sayed abdul <sakarim9124@gmail.com>
2019-03-05 02:16:58 +0530

    Updates

---
+++ precache-manifest.429f23c490d07b8c035ab6fc92a35259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger_Builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "2b0daacd39cd3945bba7",
+    "url": "/Burger_Builder/static/js/runtime~main.2b0daacd.js"
+  },
+  {
+    "revision": "7d69368fca927e6aae66",
+    "url": "/Burger_Builder/static/js/main.7d69368f.chunk.js"
+  },
+  {
+    "revision": "d81d15f0059300dd36ac",
+    "url": "/Burger_Builder/static/js/2.d81d15f0.chunk.js"
+  },
+  {
+    "revision": "7d69368fca927e6aae66",
+    "url": "/Burger_Builder/static/css/main.6d502367.chunk.css"
+  },
+  {
+    "revision": "ddfdba8a3ab2091e98c9cb799c336744",
+    "url": "/Burger_Builder/index.html"
+  }
+];
